3d2f4cffb92968899bac9e3f19227207e89728ac
Martijn Hamstra <martijnhamstra@camasit.nl>
2021-11-26 16:00:42 +0100

    removed length check

--- library/src/main/java/com/scanlibrary/PickImageFragment.java
+++ library/src/main/java/com/scanlibrary/PickImageFragment.java
@@ -247,7 +247,7 @@ public class PickImageFragment extends Fragment {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == MY_CAMERA_REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 openCamera();
             } else {
                 Toast.makeText(getActivity(), "camera permission denied", Toast.LENGTH_LONG).show();
